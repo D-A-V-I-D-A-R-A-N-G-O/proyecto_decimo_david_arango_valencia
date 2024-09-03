060646507e66e0aa17ffe7232d5e69741fc8fc27
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2024-09-03 18:40:39 -0500

    cambios
    cambios hechos durante la clase del día 2

--- backend/models/modelusuario.js
+++ backend/models/modelusuario.js
@@ -10,10 +10,22 @@ const getAllUsuarios = (nombre, email, callback)=>{
     return db.run(sql, [nombre, email], callback)
 }
 const getUsuarioById = (id, callback) => {
-    const sql = 'SELECT * FROM Usuario WHERE Usuario.id = 1'
+    const sql = 'SELECT * FROM Usuario WHERE Usuario.id = ?'
+    return db.run(sql, [id], callback)
+}
+const UpdateUsuario = (id, nombre, email, callback)=> {
+    const sql = 'UPDATE Usuario SET nombre = ?, email = ? WHERE id = ?'
+    return db.run(sql, [id, nombre, email], callback)
+}
+const deleteUsuario = (id, callback) => {
+    const sql = 'DELETE FROM Usuario WHERE Usuario.id = ?'
+    return db.run(sql, [id], callback)
 }
 
 module.exports = {
     createUsuario,
-    getAllUsuarios
+    getAllUsuarios,
+    getAllUsuarios,
+    UpdateUsuario,
+    deleteUsuario
 }
