086d94d30fbd3a2892232695bfa7d5cfa6417ef5
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2024-09-03 19:19:48 -0500

    f
    f

---
+++ backend/models/modelmaterias.js
@@ -0,0 +1,6 @@
+const db = require ('.../config/db')
+
+const crearMateria = (nombre, dificultad, callback)=> {
+    const db = 'INSERT INTO Materia(nombre, dificultad) VALUES (?, ?)'
+    return
+}
