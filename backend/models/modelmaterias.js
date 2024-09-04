7850ef8c0cc1137cc55eb5cd3b04d383f077afd4
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2024-09-03 19:50:45 -0500

    d
    d

--- backend/models/modelmaterias.js
+++ backend/models/modelmaterias.js
@@ -2,5 +2,18 @@ const db = require ('.../config/db')
 
 const crearMateria = (nombre, dificultad, callback)=> {
     const db = 'INSERT INTO Materia(nombre, dificultad) VALUES (?, ?)'
-    return
+    return sql.run(sql, [nombre, dificultad], callback)
+}
+const CallAllMaterias = (nombre, dificultad, callback)=>{
+    const db = 'SELECT*FROM Materia'
+    return sql.run(sql, [nombre, dificultad], callback)
+}
+const CallMateriaById = (id, callback) => {
+    const db = 'SELECT*FROM Materia WHERE Materia.id=?'
+    return sql.run(sql, [id], callback)
+}
+module.exports = {
+    crearMateria,
+    CallAllMaterias,
+    CallMateriaById
 }
